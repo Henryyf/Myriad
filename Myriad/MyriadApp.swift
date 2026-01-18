3de811bd46b310040eef73500ae8639d38f850c9
Henryyf <jhong34@u.rochester.edu>
2026-01-18 22:28:21 +0800

    Initial Commit

---
+++ Myriad/MyriadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyriadApp.swift
+//  Myriad
+//
+//  Created by 洪嘉禺 on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MyriadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
